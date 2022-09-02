dcc357cc350c6903566a6fdfa2c3cb85184447d1
Ubuntu <ubuntu@ip-172-31-30-229.ec2.internal>
2022-09-02 14:51:52 +0000

    Fix integration_test to run from parent directory

--- web-service/integration_test/run.sh
+++ web-service/integration_test/run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "${LOCAL_IMAGE_NAME}" == "" ]; then 
+if [[ -z "${GITHUB_ACTIONS}" ]]; then
+  cd "$(dirname "$0")"
+fi
+
+if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
     LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
     export LOCAL_IMAGE_NAME="house-price-prediction-service:${LOCAL_TAG}"
     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
